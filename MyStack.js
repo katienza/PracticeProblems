4f8d06da90ec064fbbf4c6bb758d3fb8be05e82c
Ken Atienza <atienza.ken@gmail.com>
2019-05-05 20:33:11 -0700

    Implements solution for creating a stack data structure.

--- MyStack.js
+++ MyStack.js
@@ -1,4 +1,4 @@
-class myStack {
+/* class myStack {
   constructor() {
     this.items = []
   }
@@ -22,6 +22,32 @@ class myStack {
   size() {
     return this.items.length
   }
+}
+
+let stack = new myStack() */
+
+const myStack = function () {
+  this.items = []
+}
+
+myStack.prototype.add = function(value) {
+  this.items.push(value)
+}
+
+myStack.prototype.remove = function() {
+  this.items.pop()
+}
+
+myStack.prototype.peek = function() {
+  return this.items[this.items.length - 1]
+}
+
+myStack.prototype.isEmpty = function() {
+  return this.items.length === 0
+}
+
+myStack.prototype.size = function() {
+  return this.items.length
 }
 
 let stack = new myStack()
