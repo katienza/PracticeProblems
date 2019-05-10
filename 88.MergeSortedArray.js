eaef2e12e08f086216b435575897df589e235798
Ken Atienza <atienza.ken@gmail.com>
2019-05-10 13:34:01 -0700

    Refactored merge and added merge of two sorted arrays used in mergeSort.

--- 88.MergeSortedArray.js
+++ 88.MergeSortedArray.js
@@ -1,3 +1,33 @@
+/*** Merging two sorted arrays for mergeSort ***/
+const merge = (arr1, arr2) => {
+  let results = []
+  let i = 0
+  let j = 0
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      results.push(arr1[i])
+      i++
+    } else {
+      results.push(arr2[j])
+      j++
+    }
+  }
+
+  while (i < arr1.length) {
+    results.push(arr1[i])
+    i++
+  }
+
+  while (j < arr2.length) {
+    results.push(arr2[j])
+    j++
+  }
+
+  return results
+}
+
+/*** LeetCode problem set  ***/
 const merge = function(nums1, m, nums2, n) {
   let index = m + n - 1
   let i = m - 1
