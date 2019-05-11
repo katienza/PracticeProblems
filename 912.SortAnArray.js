8a9cd62bbda5ffad09a94ce2b007320c442ee26a
Ken Atienza <atienza.ken@gmail.com>
2019-05-11 11:20:08 -0700

    Implements solution for Sort an Array problem set on LeetCode.

---
+++ 912.SortAnArray.js
@@ -0,0 +1,37 @@
+const merge = (arr1, arr2) => {
+  let results = []
+  let i = 0
+  let j = 0
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr[i] < arr[j]) {
+      results.push(arr1[i])
+      i++
+    } else {
+      results.push(arr2[j])
+      j++
+    }
+  }
+
+  while (i < arr1.length) {
+    results.push(arr1[i])
+    i++
+  }
+
+  while (j < arr2.length) {
+    results.push(arr2[j])
+    j++
+  }
+
+  return results
+}
+
+const sortArray = nums => {
+  if (nums.length === 0) return nums
+
+  let mid = Math.floor(nums.length / 2)
+  let left = sortArray(nums.slice(0, mid))
+  let right = sortArray(nums.slice(mid))
+
+  return merge(left, right)
+}
