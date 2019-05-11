d3b7ef69050da969e543f9fe2935d257ff158cff
Ken Atienza <atienza.ken@gmail.com>
2019-05-11 14:02:35 -0700

    Refactored TwoSum problem set and added a version with O(n) time & space complexity.

--- 1.TwoSum.js
+++ 1.TwoSum.js
@@ -1,13 +1,27 @@
+// time complexity: O(n), space complexity: O(n)
+const twoSum = (nums, target) => {
+  let map = {};
+
+  for(let i = 0; i < nums.length; i++) {
+    let num = nums[i]
+
+    if(map[num] != undefined){
+      return [map[num], i]
+    } else {
+      map[target - num] = i
+    }
+  }
+
+  return []
+}
+
+// time complexity: O(n^2), space complexity: O(n)
 var twoSum = function(nums, target) {
-  let result = [];
-      
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
       if (nums[i] + nums[j] === target) {
-        result.push(i, j)
+        return [i,j]
       }
     }
   }
-  
-  return result
 };
